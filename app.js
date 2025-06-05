55d18f3194d82cec51bb6215ba6d9af7fb82751c
Onlook <git@onlook.com>
2025-06-05 23:23:34 +0300

    How can I resolve these errors? If you propose a fix, please make it concise.

--- app.js
+++ app.js
@@ -130,7 +130,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
